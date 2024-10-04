0c944426b1fa8a68ba89ead7bffafb0d47cd791f
kikuuchi <kikuuchi@itcowork.co.jp>
2024-10-04 17:52:21 +0900

    feat: 意見一覧画面を表示する処理の実装

--- app/suggestion/page.tsx
+++ app/suggestion/page.tsx
@@ -1,7 +1,22 @@
-export default function SuggestionPage() {
+import { Suggestions } from "@/apis/suggestion/types";
+import { Button } from "@mui/material";
+import { getSuggestions } from "@/apis/suggestion/api/getSuggestions";
+import SuggestionList from "@/components/pages/suggestion/suggestionList/suggestionList";
+
+export default async function SuggestionPage() {
+  const { status, data } = await getSuggestions();
+  const suggestions: Suggestions = data;
+
   return (
-      <div>
-        <div>意見一覧画面</div>
+    <div className="flex justify-center mt-12 w-full">
+      <div className="flex flex-col max-w-[800px] w-full mx-auto">
+        <div className="self-start">
+          <Button variant="contained" className="w-42 mb-8 bg-neutral-500">
+            意見を投稿
+          </Button>
+        </div>
+        <SuggestionList suggestions={suggestions} />
       </div>
+    </div>
   );
 }
