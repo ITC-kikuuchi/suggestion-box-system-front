df60597a6638d628a3559857af82be2a68eba1b9
kikuuchi <kikuuchi@itcowork.co.jp>
2024-10-12 22:07:49 +0900

    fix: api の url を定数の値を使うように修正

--- apis/suggestion/api/getSuggestions.ts
+++ apis/suggestion/api/getSuggestions.ts
@@ -1,9 +1,10 @@
 import { cookies } from 'next/headers';
+import { API_URL } from '@/lib/config';
 
 export const getSuggestions = async () => {
   const token = cookies().get('token')?.value;
   // 意見一覧取得API の実行
-  const response = await fetch("http://localhost/suggestions", {
+  const response = await fetch(`${API_URL}/suggestions`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
