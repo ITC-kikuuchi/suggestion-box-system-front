fe0102ffe9aa06c08a4bec0f0ef136c07faef549
kikuuchi <kikuuchi@itcowork.co.jp>
2024-10-20 16:50:03 +0900

    refactor: コード整形

--- apis/suggestion/api/getSuggestions.ts
+++ apis/suggestion/api/getSuggestions.ts
@@ -2,9 +2,9 @@ import { API_URL } from '@/lib/config';
 import { parse } from 'cookie';
 
 export const getSuggestions = async () => {
+
   // document.cookie をパースしてオブジェクトとして取得
   const cookies = parse(document.cookie);
-
   // 意見一覧取得API の実行
   const response = await fetch(`${API_URL}/suggestions`, {
     method: "GET",
