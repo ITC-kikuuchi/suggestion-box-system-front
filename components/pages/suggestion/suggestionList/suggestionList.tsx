7d1c33c4a5abaf66794e183c12648cdcd35fd300
kikuuchi <kikuuchi@itcowork.co.jp>
2024-10-20 16:55:22 +0900

    fix: 意見一覧表示処理の修正

--- components/pages/suggestion/suggestionList/suggestionList.tsx
+++ components/pages/suggestion/suggestionList/suggestionList.tsx
@@ -3,10 +3,12 @@ import SuggestionCard from "../suggestionCard/suggestionCard";
 
 export default function SuggestionList({ suggestions }: { suggestions: Suggestions }) {
   return (
-    <div>
-      {suggestions.suggestion_list.map((suggestion) => (
-        <SuggestionCard key={suggestion.id} suggestion={suggestion} />
-      ))}
+    <div className="max-h-[500px] overflow-y-auto">
+      <div className="pr-2">
+        {suggestions.suggestion_list.map((suggestion) => (
+          <SuggestionCard key={suggestion.id} suggestion={suggestion} />
+        ))}
+      </div>
     </div>
   );
 }
