f49de1ebf410c913f9b4a2cab0095494a5fb5dd1
kikuuchi <kikuuchi@itcowork.co.jp>
2024-11-24 02:11:18 +0900

    fix: 1536px以上の画面の場合にレスポンシブ対応を行う処理の実装

--- components/pages/suggestion/suggestionList/suggestionList.tsx
+++ components/pages/suggestion/suggestionList/suggestionList.tsx
@@ -3,7 +3,7 @@ import SuggestionCard from "../suggestionCard/suggestionCard";
 
 export default function SuggestionList({ suggestions }: { suggestions: Suggestions }) {
   return (
-    <div className="max-h-[500px] overflow-y-auto">
+    <div className="max-h-[500px] 2xl:max-h-[700px] overflow-y-auto">
       <div className="pr-2">
         {suggestions.suggestion_list.map((suggestion) => (
           <SuggestionCard key={suggestion.id} suggestion={suggestion} />
